8f4a10db320cec38b2ecda7c6d736ba464aaf419
Sibyl <scauyb@qq.com>
2025-12-19 14:40:58 +0800

    feat(log): 初始化日志记录器并添加测试
    - 添加了日志记录器初始化逻辑，确保只初始化一次
    - 集成了环境变量配置和自定义日志格式
    - 日志格式包含时间戳、模块路径、行号和日志级别
    - 添加了测试模块以验证日志记录功能
    - 在测试中添加了"hello world"日志输出验证

--- crates/utils/src/tools/log.rs
+++ crates/utils/src/tools/log.rs
@@ -1,18 +1,39 @@
+use std::io::Write;
 use std::sync::Once;
 
 static INIT: Once = Once::new();
 /// because logger can only init once, so we use Once to ensure it
 pub fn init_logger() {
-    INIT.call_once(env_logger::init);
+    INIT.call_once(|| {
+        dotenvy::dotenv().ok();
+        env_logger::Builder::from_default_env()
+            .format(|buf, record| {
+                writeln!(
+                    buf,
+                    "[{} {}:{} {}] {}",
+                    chrono::Local::now()
+                        .format("%Y-%m-%d %H:%M:%S"),
+                    record
+                        .module_path()
+                        .unwrap_or("<unnamed>"),
+                    record.line().unwrap_or(0),
+                    record.level(),
+                    record.args()
+                )
+            })
+            .init();
+    });
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use log::info;
 
     #[test]
     fn test_1() -> anyhow::Result<()> {
         init_logger();
+        info!("hello world");
         Ok(())
     }
 }
